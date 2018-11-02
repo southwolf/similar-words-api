7702b919356674326ea400f448f2a788a2e94e16
Rick Song <risong@xogrp.com>
2018-11-02 16:34:44 +0800

    update deploy

--- scripts/k8s/deploy.sh
+++ scripts/k8s/deploy.sh
@@ -7,10 +7,10 @@ INGRESS_HOST="${ENV_PREFIX}${CNAME}"
 docker run --rm xogroup/xo-helm:$K8_CLUSTER \
    upsert $APPLICATION-$ENV infrastructure/xo-app-manifest \
    --set replicaCount=1 \
-   --set image.repository=$REPOSITORY \
-   --set image.tag=$TAG \
+   --set image.repository=southwolf/similar-words-api \
+   --set image.tag=latest \
    --set image.pullPolicy=Always \
-   --set service.internalPort=80 \
+   --set service.internalPort=5000 \
    --set service.matchSelectors.application=$APPLICATION \
    --set service.matchSelectors.environment=$ENV \
    --set service.matchSelectors.role=$ROLE \
@@ -20,10 +20,11 @@ docker run --rm xogroup/xo-helm:$K8_CLUSTER \
    --set labels.application=$APPLICATION \
    --set ingress.enabled=true \
    --set ingress.hosts={$INGRESS_HOST} \
-   --set "env[0].name=FIRST_ENV_VARIABLE_NAME,env[0].value=first_env_variable_value" \
-   --set resources.requests.cpu=1 \
+   --set ingress.tls.hosts={$INGRESS_HOST} \
+   --set "env[0].name=FLASK_ENV,env[0].value=development" \
+   --set resources.requests.cpu=0.1 \
    --set resources.limits.cpu=1 \
    --set resources.requests.memory=2G \
-   --set resources.limits.memory=2G \
+   --set resources.limits.memory=4G \
    --set imagePullSecrets.name=$K8_DOCKER_SECRET \
    --namespace $NAMESPACE
